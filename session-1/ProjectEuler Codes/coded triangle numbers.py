b8e9a8dcd54e8448b1936c2d33d45c0b8b41c3d0
RusiraVindiya <rusiravk@gmail.com>
2025-10-08 10:09:23 +0530

    New file added to ProjectEuler Codes

--- session-1/ProjectEuler Codes/coded triangle numbers.py
+++ session-1/ProjectEuler Codes/coded triangle numbers.py
@@ -1,9 +1,3 @@
-ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-POS = {ch: i+1 for i, ch in enumerate(ALPHA)}
-words = []
-with open('words.txt', 'r') as f:
-    words = f.read().replace('"', '').split(',')
-
 def isTriangleNumber(n):
     x = (8*n + 1)**0.5
     return x.is_integer() and (x - 1) % 2 == 0
@@ -12,8 +6,12 @@ def isCodedTriangleNumber(word):
     total = sum(POS[ch] for ch in word)
     return isTriangleNumber(total)
 
-count = sum(1 for word in words if isCodedTriangleNumber(word))
-print(count)
+with open('words.txt', 'r') as file:
+    content = file.read()
+    words = content.replace('"', '').split(',')
+    POS = {ch: i+1 for i, ch in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ')}
+    count = sum(1 for word in words if isCodedTriangleNumber(word))
+    print(count)
 
-#Time Complexity: O(n*m)
-#Space Complexity: O(1)
+# Time Complexity: O(n * m)
+# Space Complexity: O(1)
